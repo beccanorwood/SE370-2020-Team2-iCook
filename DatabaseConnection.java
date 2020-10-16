8465b94e3d565761d56bc372048ce0e614edeb6c
Izzy <71158214+Izzyxiii@users.noreply.github.com>
2020-10-16 16:19:25 -0700

    adding the driver class

---
+++ DatabaseConnection.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String username = "se370";
+    private static final String password = "Team2!";
+    private static final String database = "icook";
+    private static final String url = "jdbc:mysql://izzy-se370.ca3u8x8hrfhy.us-west-1.rds.amazonaws.com:3306/" + database;
+
+    public Statement statement;
+
+    // Constructor
+    DatabaseConnection() throws SQLException {
+        // Register the Driver
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+
+        // Getting the connection
+        Connection connection = DriverManager.getConnection(url, username, password);
+        System.out.println("Connection established....");
+
+        // Creating a statement object
+        statement = connection.createStatement();
+    }
+}
---
+++ Driver.java
@@ -0,0 +1,29 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+/**
+ * Driver
+ *
+ * @author Team 2
+ * @version 10/16/2020
+ */
+public class Driver
+{
+    public static void main(String[] args) throws SQLException
+    {
+        // create new database object
+        DatabaseConnection database = new DatabaseConnection();
+
+        // Insert an item to the database
+        database.statement.execute("INSERT INTO test_izzy (first_name) VALUE ('test')");
+
+        // Retrieving the data
+        ResultSet resultSet = database.statement.executeQuery("SELECT COUNT(*) FROM test_izzy");
+        resultSet.next();
+
+        // Moving the cursor to the last row
+        System.out.println("Table contains " + resultSet.getInt("COUNT(*)") + " rows");
+    }
+}
