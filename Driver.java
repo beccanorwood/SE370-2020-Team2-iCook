5999aa6e838772608b2d8ad00f03d8013513eb28
Izzy <71158214+Izzyxiii@users.noreply.github.com>
2020-11-06 17:15:01 -0800

    hi

--- Driver.java
+++ Driver.java
@@ -14,19 +14,19 @@ public class Driver
         DatabaseConnection database = new DatabaseConnection();
 
         // Insert an item into the ingredients table
-        //database.addIngredient("milk", 2);
+        //database.addUserIngredient(1, 2, 50);
 
         // Update an item in the ingredients table
-        //database.updateIngredient("milk", 9);
+        //database.updateUserIngredient(1, 9);
 
         // Remove an item from the ingredients table
-        //database.deleteIngredient("milk");
+        //database.deleteUserIngredient(2);
 
         // Retrieving the data
-        System.out.println("My Ingredients:");
-        database.displayIngredients();
+        //System.out.println("iCook Ingredients:");
+        //database.displayIngredientsTable();
 
         // get the quantity of a specific ingredient
-        System.out.print("\nAmount of sugar: " + database.getIngredientQuantity("sugar"));
+        //System.out.print("\nSugar is measure in: " + database.getIngredientMeasurement(1));
     }
 }
