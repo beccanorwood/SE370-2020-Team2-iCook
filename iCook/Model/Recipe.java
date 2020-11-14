cc85c529c76957eb55469b2db0f47eb9f05a0f5c
Ben Dominguez <domin217@cougars.csusm.edu>
2020-11-14 11:51:47 -0800

    recipe and RecipeIngredient

--- iCook/Model/Recipe.java
+++ iCook/Model/Recipe.java
@@ -1,4 +1,53 @@
 package iCook.Model;
+import java.util.List;
 
+/**
+ * Recipe creates an object that holds relevant information including the name, id number, a list of
+ * RecipeIngredient objects, as well as the number of servings a given recipe will produce
+ */
 public class Recipe {
+
+    private int recipeID;                           // id number associated with given recipe in database
+    private String recipeName;                      // name of recipe
+    private List<RecipeIngredient> rcpIngredients;  // a list of RecipeIngredient objects required for recipe
+    private int numServings;                        // number of servings recipe will produce
+
+    public Recipe(int recipeID, String recipeName, List<RecipeIngredient> rcpIngredients, int numServings) {
+        this.recipeID = recipeID;
+        this.recipeName = recipeName;
+        this.rcpIngredients = rcpIngredients;
+        this.numServings = numServings;
+    }
+
+    public int getRecipeID() {
+        return recipeID;
+    }
+
+    public void setRecipeID(int recipeID) {
+        this.recipeID = recipeID;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public List<RecipeIngredient> getRcpIngredients() {
+        return rcpIngredients;
+    }
+
+    public void setRcpIngredients(List<RecipeIngredient> rcpIngredients) {
+        this.rcpIngredients = rcpIngredients;
+    }
+
+    public int getNumServings() {
+        return numServings;
+    }
+
+    public void setNumServings(int numServings) {
+        this.numServings = numServings;
+    }
 }
---
+++ iCook/Model/RecipeIngredient.java
@@ -0,0 +1,59 @@
+package iCook.Model;
+
+/**
+ * RecipeIngredient creates ingredient objects that hold information about type, and quantity of ingredients required
+ * for a given recipe
+ */
+public class RecipeIngredient {
+
+        private int ingredientID;   // ID associated with given ingredient within the database
+        private int recipeID;       // ID associated with given recipe within the database
+        private int rcpIngredientID;// ID associated with given recipeIngredient within the database
+        private float quantity;     // number of ingredient type necessary
+
+    /**
+     * Constructor
+     * @param ingredientID
+     * @param recipeID
+     * @param rcpIngredientID
+     * @param quantity
+     */
+    public RecipeIngredient(int ingredientID,  int recipeID, int rcpIngredientID, float quantity) {
+        this.ingredientID = ingredientID;
+        this.recipeID = recipeID;
+        this.rcpIngredientID = rcpIngredientID;
+        this.quantity = quantity;
+    }
+
+    public int getRecipeIngredientID() {
+        return ingredientID;
+    }
+
+    public void setRecipeIngredientID(int ingredientID) {
+        this.ingredientID = ingredientID;
+    }
+
+    public int getRecipeID() {
+        return recipeID;
+    }
+
+    public void setRecipeID(int recipeID) {
+        this.recipeID = recipeID;
+    }
+
+    public int getRcpIngredientID() {
+        return rcpIngredientID;
+    }
+
+    public void setRcpIngredientID(int rcpIngredientID) {
+        this.rcpIngredientID = rcpIngredientID;
+    }
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+}
