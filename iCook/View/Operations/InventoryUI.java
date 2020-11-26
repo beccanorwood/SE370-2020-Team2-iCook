c5eadc9ab1507cbb5edf75ccc6fc36d911e1bf8a
beccanorwood <rebeccarosenorwood@gmail.com>
2020-11-25 16:41:40 -0800

    Added remove buttons for each item that user adds

--- iCook/View/Operations/InventoryUI.java
+++ iCook/View/Operations/InventoryUI.java
@@ -6,11 +6,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class InventoryUI extends JFrame implements ActionListener {
 
     //Display dropbox containing list of available ingredients
     //User selects and it gets added to their inventory
+    private int numofIngredients = 1;
     private JFrame userInventory;;
     private JPanel topPanel;
     private JPanel bottomPanel;
@@ -26,6 +29,9 @@ public class InventoryUI extends JFrame implements ActionListener {
     private Checkbox name;
     private JComboBox userIngredients;
     private Box box;
+    private Box button_box;
+    private HashMap<JButton, Box> delete = new HashMap<JButton, Box>();
+    private ArrayList<String> iCookIngredients = new ArrayList<>();
 
 
     public InventoryUI(){
@@ -38,10 +44,16 @@ public class InventoryUI extends JFrame implements ActionListener {
         //Current ingredients
          ingredients = new String[]{"sugar", "milk", "salt", "chicken", "bacon",
          "pepper", "butter", "sunflower oil", "onion", "carrot", "mined beef",
-         "mushrooms", "flour", "beef stock cube", "mashed potato", "butter",
-         "tomatoes", "eggs", "milk", "potatoes"};
+         "mushrooms", "flour", "beef stock cube", "mashed potato", "tomatoes",
+          "eggs", "milk", "potatoes"};
 
-         userIngredients = new JComboBox(ingredients);
+         for(int i = 0; i < ingredients.length; i++){
+             iCookIngredients.add(ingredients[i]);
+         }
+
+         Collections.sort(iCookIngredients);
+
+         userIngredients = new JComboBox(iCookIngredients.toArray());
          userIngredients.addActionListener(this);
 
          title = new JLabel("Your Ingredient Inventory");
@@ -49,11 +61,10 @@ public class InventoryUI extends JFrame implements ActionListener {
          title.setForeground(Color.WHITE);
 
          add = new JButton("Add");
-         remove = new JButton("Remove");
+         //remove = new JButton("Remove");
          search = new JButton("Search");
          searchAll = new JButton("Search All");
          add.addActionListener(this);
-         remove.addActionListener(this);
          search.addActionListener(this);
          searchAll.addActionListener(this);
 
@@ -66,13 +77,13 @@ public class InventoryUI extends JFrame implements ActionListener {
          ingredientPanel.setBackground(Color.BLACK);
 
          box = Box.createVerticalBox();
+         button_box = Box.createVerticalBox();
 
          chosenIngredients = new JPanel();
          chosenIngredients.setBackground(Color.BLACK);
          chosenIngredients.setForeground(Color.WHITE);
 
          rightPanel = new JPanel();
-         rightPanel.add(remove);
          rightPanel.setBackground(Color.BLACK);
 
          bottomPanel = new JPanel();
@@ -83,27 +94,55 @@ public class InventoryUI extends JFrame implements ActionListener {
          userInventory.add(topPanel, BorderLayout.NORTH);
          userInventory.add(ingredientPanel, BorderLayout.WEST);
          userInventory.add(chosenIngredients, BorderLayout.CENTER);
-         userInventory.add(rightPanel, BorderLayout.EAST);
          userInventory.add(bottomPanel, BorderLayout.SOUTH);
 
          userInventory.setVisible(true);
+    }
+
+    private void DeletePanel(int numofIngredients)
+    {
+        //Everytime a user selects an ingredient from the dropdown list
+        //A new remove button will be added to the rightmost panel
+        ArrayList<JButton> removeIng = new ArrayList<JButton>();
+
+        for(int i = 0; i < numofIngredients; i++) {
+            JButton button = new JButton();
+            button.setText("Remove");
+            removeIng.add(button);
+            rightPanel.add(removeIng.get(i));
+            button_box.add(removeIng.get(i));
+            removeIng.get(i).addActionListener(this);
+        }
+
+        rightPanel.add(button_box);
+        userInventory.add(rightPanel, BorderLayout.EAST);
+        userInventory.setVisible(true);
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        ArrayList<Checkbox> selectedIng = new ArrayList<Checkbox>();
 
         JComboBox src2 = (JComboBox) e.getSource();
 
-        Object selected = src2.getSelectedItem();
-        System.out.println("Selected Item: " + selected);
+        //Loop to check through list of ingredients
+        //If ingredient is selected, it will be added to center panel
+        for(int i = 0; i < ingredients.length; i++){
+            if(src2.getSelectedItem() == (ingredients[i])){
 
-        name = new Checkbox((String) selected);
-        name.setForeground(Color.WHITE);
-        box.add(name);
+                System.out.println("Selected Item: " + ingredients[i]);
 
-        chosenIngredients.add(box);
-        userInventory.setVisible(true);
+                name = new Checkbox((String) ingredients[i]);
+                name.setForeground(Color.WHITE);
+                box.add(name);
 
+                selectedIng.add(name); //Add to list
+
+                chosenIngredients.add(box);
+
+                DeletePanel(numofIngredients);
+            }
+        }
     }
 }
--- iCook/View/Operations/RecipeUI.java
+++ iCook/View/Operations/RecipeUI.java
@@ -2,4 +2,7 @@ package iCook.View.Operations;
 
 public class RecipeUI {
 
+    //GUI where user can view all recipes from iCook's database
+    //Search and filter
+
 }
